53021c0984eb1cdc7153116965f7d1cb6dfb665a
Nikolay Derkach <nderk@me.com>
2014-05-29 15:14:05 -0700

    init

--- PopTestAnimation/TraffleViewController.h
+++ PopTestAnimation/TraffleViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TraffleViewController : UIViewController
+@interface TraffleViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
--- PopTestAnimation/TraffleViewController.m
+++ PopTestAnimation/TraffleViewController.m
@@ -8,7 +8,12 @@
 
 #import "TraffleViewController.h"
 
+#import <POP.h>
+
+static int delay = 0;
+
 @interface TraffleViewController ()
+
 
 @end
 
@@ -25,5 +30,43 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell
+    cell.textLabel.text = @"test";
+    cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    
+    CGFloat toValue = CGRectGetMidX(self.view.bounds);
+
+    POPSpringAnimation *onscreenAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionX];
+    onscreenAnimation.fromValue = @(-toValue);
+    onscreenAnimation.toValue = @(toValue);
+    onscreenAnimation.springBounciness = 5.f;
+    onscreenAnimation.beginTime = (CACurrentMediaTime() + delay);
+    delay++;
+
+    [cell.layer pop_addAnimation:onscreenAnimation forKey:onscreenAnimation.name];
+
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
 
 @end
